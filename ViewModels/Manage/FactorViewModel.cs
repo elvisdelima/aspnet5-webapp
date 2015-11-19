05753a5d00160cb6e1b3aa75577094d89af9594a
elvislima <elvislima@gmail.com>
2015-11-19 16:55:47 -0300

    Commit inicial

---
+++ ViewModels/Manage/FactorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace aspnet5_webapp.ViewModels.Manage
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}
